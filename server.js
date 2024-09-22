3df2cd865c995ff099148f519a30a826a39b25f0
FemiBam <Femibam1@gmail.com>
2024-09-22 16:15:17 -0400

    update homepage to homePage

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/auth", authController);
 app.use("/players", playerController);
 app.use("/roster", rosterController);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Listening on port 3000");
 });
 
